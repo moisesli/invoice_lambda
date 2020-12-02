c9708592b100dcc084c0ae12f907797fb289023f
moises <moises@iMac-Pro-de-moises.local>
2020-12-01 22:59:33 -0500

    edit pug engine render

--- lambda.js
+++ lambda.js
@@ -1,7 +1,7 @@
 // lambda.js
 'use strict'
 const awsServerlessExpress = require('aws-serverless-express')
-const app = require('./src/app.js')
+const app = require('./app')
 const server = awsServerlessExpress.createServer(app)
  
 exports.handler = (event, context) => { awsServerlessExpress.proxy(server, event, context) }
