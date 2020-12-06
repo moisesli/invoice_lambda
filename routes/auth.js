4d184c9f6dad298f84dbf35c2b28daf7f129774f
moises <elnaufrago2009@gmail.com>
2020-12-05 19:51:40 -0500

    edit

---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var router = express.Router()
+
+// 
+router.get('/auth/login', function(req, res, next){
+
+});
+
+router.get('/auth/login_autenticate', function(req, res, next){
+    
+});
+
+router.get('/auth/register', function(req, res, next){
+    
+});
+
+router.get('/auth/register_autenticate', function(req, res, next){
+    
+});
